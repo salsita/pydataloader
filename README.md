f80fe580f3c774e0cae079f04b0d6fc31a2a9e65
Jan Killian <jan.killian@gmail.com>
2015-02-25 17:32:55 +0100

    feat: initial release

---
+++ README.md
@@ -0,0 +1,49 @@
+# [PyDataLoader](https://github.com/salsita/pydataloader) <a href='https://github.com/salsita'><img align='right' title='Salsita' src='https://www.google.com/a/cpanel/salsitasoft.com/images/logo.gif?alpha=1' /></a>
+
+Extensible JSON/YAML/RAML/... data loader.
+
+[![Latest Version](https://pypip.in/version/PyDataLoader/badge.svg)]
+(https://pypi.python.org/pypi/PyDataLoader/)
+[![Downloads](https://pypip.in/download/PyDataLoader/badge.svg)]
+(https://pypi.python.org/pypi/PyDataLoader/)
+[![Supported Python versions](https://pypip.in/py_versions/PyDataLoader/badge.svg)]
+(https://pypi.python.org/pypi/PyDataLoader/)
+[![License](https://pypip.in/license/PyDataLoader/badge.svg)]
+(https://pypi.python.org/pypi/PyDataLoader/)
+
+
+## Supported Platforms
+
+* [Python](http://www.python.org/) >= 2.6, 3.3
+
+
+## Get Started
+
+Install using [pip](https://pip.pypa.io/) or [easy_install](http://pythonhosted.org/setuptools/easy_install.html):
+```bash
+pip install PyDataLoader
+easy_install PyDataLoader
+```
+
+Optionally, you can specify `yaml` or `raml` extras to install related dependencies:
+```bash
+pip install "PyDataLoader[yaml,raml]"
+easy_install "PyDataLoader[yaml,raml]"
+```
+
+## Features
+
+- Load data stored in any of supported markup languages.
+  - Support [YAML](http://yaml.org/) using [PyYAML](http://pyyaml.org/wiki/PyYAML).
+  - Support [RAML](http://raml.org/) using [pyraml-parser](https://github.com/an2deg/pyraml-parser).
+  - Support [JSON](http://json.org/) using [Python 2.6+ json module](https://docs.python.org/2/library/json.html), or [Python 3.x json module](https://docs.python.org/3/library/json.html).
+- Allow extending supported markup languages.
+- Allow extending with data postprocessing.
+
+## Changelog
+
+### 0.1.0
+
+#### Features
+
+- Initial release.
---
+++ dataloader.py
@@ -0,0 +1,71 @@
+"""Extensible JSON/YAML/RAML/... data loader."""
+
+__all__ = 'Loader'.split()
+__version__ = '0.1.0'
+
+from os.path import splitext, dirname
+
+class Loader(object):
+    markups = dict(
+        json = 'json',
+        yaml = 'yaml',
+        yml  = 'yaml',
+        raml = 'raml',
+        )
+    default_markup = 'yaml'
+
+    log = None
+    name = 'data'
+
+    def __init__(self, name=None, default_markup=None, **options):
+        if name is not None:
+            self.name = name
+        if default_markup is not None:
+            self.default_markup = default_markup
+        if options:
+            self.__dict__.update(options)
+
+    def load(self, f, markup=None, name=None, **options):
+        path = f if isinstance(f, basestring) else f.name
+        if markup is None:
+            markup = self.markups.get(splitext(path)[-1][1:], self.default_markup)
+        if self.log:
+            self.log.info('get %s: %s [%s]', name or self.name, path, markup)
+        return self.postprocess(getattr(self, 'load_{}'.format(markup))(f), name, **options)
+
+    __call__ = load
+
+    def postprocess(self, data, name=None, path=None):
+        if self.log:
+            self.log.info('got %s: %s%s', name or self.name, type(data).__name__,
+                ' [%d]' % len(data) if hasattr(data, '__len__') else '')
+        return data
+
+    def load_raml(self, f):
+        if isinstance(f, basestring):
+            from pyraml.parser import load
+            return load(f)
+        else:
+            from pyraml.parser import parse
+            return parse(f.read(), dirname(f.name))
+
+    def load_yaml(self, f):
+        from yaml import load
+        if isinstance(f, basestring):
+            with file(f, 'r') as fp:
+                return load(fp)
+        else:
+            return load(f)
+
+    def load_json(self, f):
+        from json import load
+        if isinstance(f, basestring):
+            with file(f, 'r') as fp:
+                return load(fp)
+        else:
+            return load(f)
+
+    def __str__(self):
+        return '{}.{}({}: *{}, {})'.format(self.__class__.__module__, self.__class__.__name__.lower(),
+            self.name, self.default_markup,
+            ', '.join(k for k in sorted(set(self.markups.values())) if k != self.default_markup))
